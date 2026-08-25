e71702bec7a378d662f19fe1ae2a2139f0b4e2cc
Rishit-ui <rajrishit24@gmail.com>
2026-08-25 09:16:05 +0530

    updated project

---
+++ for_each_loop.java
@@ -0,0 +1,11 @@
+public class for_each_loop {
+    public static void main(String[] args) {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        // Using for-each loop to iterate through the array
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+}
