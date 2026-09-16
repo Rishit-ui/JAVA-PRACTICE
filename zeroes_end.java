e3f7e62be3c0ea2e3bcd446af37f6d22ab8e11cf
Rishit-ui <rajrishit24@gmail.com>
2026-09-16 14:23:30 +0530

    changes updated

---
+++ zeroes_end.java
@@ -0,0 +1,15 @@
+public class zeroes_end {
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 3, 0, 4, 0, 5 };
+        int count = 0; // Count of non-zero elements
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                count++;
+            } else {
+                arr[i - count] = arr[i];
+            }
+        }
+
+    }
+}
